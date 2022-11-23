e5f7e26d4aa5ec2e03715db4fddfa02ce7e2754c
Mustafa <mustafahaji210504@gmail.com>
2022-11-23 17:36:00 +0530

    hellomynameismustafa

---
+++ C++course/Course1/Week1/assignment1.cc
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+  
+  bool test = atoi((argv[1]));
+  
+  
+bool my_bool;
+my_bool = true ;
+cout << my_bool << endl;
+my_bool = test ;
+cout << my_bool << endl;
+
+
+
+  
+  
+  return 0;
+  
+}
+
