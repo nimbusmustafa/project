d9511093ab63d2125b286c4b8bdb58aacf7cfa6a
nimbusmustafa <mustafa.mitmpl2022@learner.manipal.edu>
2023-02-10 23:09:24 +0530

    i

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/mustafa/Group_6_MRM/src
+# Build directory: /home/mustafa/Group_6_MRM/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("roverbot")
